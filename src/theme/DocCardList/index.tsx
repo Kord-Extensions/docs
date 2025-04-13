3cf89c7587cc1f55642bd867577b74eb54995dbe
Gareth Coles <gdude2002@gmail.com>
2025-04-13 19:07:21 +0100

    More card group work

--- src/theme/DocCardList/index.tsx
+++ src/theme/DocCardList/index.tsx
@@ -5,31 +5,46 @@ import DocCard from "@theme/DocCard";
 import type {Props} from "@theme/DocCardList";
 import {PropSidebarItem} from "@docusaurus/plugin-content-docs/lib/sidebars/types";
 
-type GroupedItems = {
-	[key: string]: PropSidebarItem[]
+interface CustomProps extends Props {
+	readonly items?: PropSidebarItem[];
+	readonly className?: string;
+	readonly descriptions?: { [key: string]: string } | undefined;
 }
 
-function DocCardListForCurrentSidebarCategory({className}: Props) {
+function DocCardListForCurrentSidebarCategory({className, descriptions}: CustomProps) {
 	const category = useCurrentSidebarCategory();
 
-	return <DocCardList items={category.items} className={className}/>;
+	return <DocCardList items={category.items} className={className} descriptions={descriptions}/>;
 }
 
-export default function DocCardList(props: Props): ReactNode {
+export function DocCardListWithDescriptions(props: CustomProps): ReactNode {
+	return DocCardList(props)
+}
+
+export default function DocCardList(props: CustomProps): ReactNode {
+	console.log(props)
+
 	const {items, className} = props;
+
+	let descriptions: { [key: string]: string }
+
+	if ("descriptions" in props) {
+		descriptions = props.descriptions
+	} else {
+		descriptions = {}
+	}
 
 	if (!items) {
 		return <DocCardListForCurrentSidebarCategory {...props} />;
 	}
 
 	let groupedItems: Map<string, PropSidebarItem[]> = new Map()
-
-	groupedItems.set("", []);
+	let defaultGroup: PropSidebarItem[] = []
 
 	filterDocCardListItems(items)
 		.forEach((item: PropSidebarItem) => {
-			if (item.customProps["card_group"] === undefined) {
-				groupedItems.get("").push(item)
+			if (item.customProps === undefined || item.customProps["card_group"] === undefined) {
+				defaultGroup.push(item)
 			} else {
 				const group = item.customProps["card_group"] as string
 
@@ -41,23 +56,62 @@ export default function DocCardList(props: Props): ReactNode {
 			}
 		});
 
-	const elements = Array.from(groupedItems).map(([key, items]) => {
-		return <>
-			<h2 style={{marginTop: "1rem"}}>{key}</h2>
+	function alphaSortString(a: string, b: string) {
+		return a.localeCompare(b)
+	}
 
-			<section className={clsx("row", className)}>
-				{items.map((item, index) => (
-					<article key={index} className="col col--6 margin-bottom--lg">
-						<DocCard item={item}/>
-					</article>
-				))}
-			</section>
-		</>
-	})
+	function alphaSortPair(a: [string, PropSidebarItem[]], b: [string, PropSidebarItem[]]) {
+		return a[0].localeCompare(b[0])
+	}
 
-	return <>
-		{
-			elements
-		}
-	</>
+	function alphaSortItem(a: PropSidebarItem, b: PropSidebarItem) {
+		return alphaSortString(a.label, b.label)
+	}
+
+	const elements = Array
+		.from(groupedItems)
+		.sort(alphaSortPair)
+		.map(([key, items]) => {
+			return <>
+				<h2
+					style={{
+						marginTop: "1rem",
+						marginBottom: descriptions[key] === undefined ? "1rem" : "0",
+					}}>{key}</h2>
+
+				{
+					descriptions[key] === undefined ? <></> :
+						<p
+							style={{
+								paddingLeft: "0.1rem", marginBottom: "1rem"
+							}}>{descriptions[key]}</p>
+				}
+
+				<section className={clsx("row", className)} style={{paddingBottom: "0"}}>
+					{items.sort(alphaSortItem).map((item, index) => (
+						<article key={index} className="col col--6 margin-bottom--lg">
+							<DocCard item={item}/>
+						</article>
+					))}
+				</section>
+			</>
+		})
+
+	elements.push(
+		// Only create the "Others" section if the default group has items.
+		defaultGroup.length === 0 ? <></> :
+			<>
+				<h2 style={{marginTop: "1rem"}}>Others</h2>
+
+				<section className={clsx("row", className)}>
+					{defaultGroup.sort(alphaSortItem).map((item, index) => (
+						<article key={`default-${index}`} className="col col--6 margin-bottom--lg">
+							<DocCard item={item}/>
+						</article>
+					))}
+				</section>
+			</>
+	)
+
+	return <>{elements}</>
 }
