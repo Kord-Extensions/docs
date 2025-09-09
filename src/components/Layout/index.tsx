7be2c349cdea1aeed664de72114e163730f8f6fb
Mercie Coles <mercie@mercie.dev>
2025-09-09 12:01:29 +0100

    Attempt some kinda mobile component style

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -6,10 +6,13 @@ import clsx, {ClassValue} from "clsx";
 type Props = {
 	style?: React.CSSProperties,
 	className?: ClassValue | ClassValue[],
+	collapse?: boolean,
 }
 
 export function Row(props: PropsWithChildren<Props>): ReactNode {
-	return <div className={clsx(styles.row, props.className)} style={props.style}>
+	let collapse = props.collapse ?? true;
+
+	return <div className={clsx(styles.row, props.className, collapse ? styles.collapse : undefined)} style={props.style}>
 		{props.children}
 	</div>
 }
