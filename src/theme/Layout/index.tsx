ed9fe57aa5f90c4561aa9bb13a16d6c93d624727
Mercie Coles <mercie@mercie.dev>
2025-12-15 12:21:32 +0000

    Remove warning banner

--- src/theme/Layout/index.tsx
+++ src/theme/Layout/index.tsx
@@ -7,13 +7,9 @@ import clsx from "clsx";
 import {Icon} from "@iconify/react"; // Import the entire Iconify library.
 import {Tooltip} from "react-tooltip"
 
-import {Icons} from "/src/icons";
-
 type Props = WrapperProps<typeof LayoutType>;
 
 export default function LayoutWrapper(props: Props): ReactNode {
-	const [isOpen, setIsOpen] = useState(true);
-
 	return (
 		<>
 			<Tooltip id="internal-api-tooltip" place="bottom" className="react-tooltip"
@@ -53,33 +49,6 @@ export default function LayoutWrapper(props: Props): ReactNode {
 
 			<Tooltip id="generic-tooltip" className="react-tooltip"
 			         classNameArrow="react-tooltip-arrow" opacity="1" />
-
-			{
-				// TODO: Remove warning on release!
-				isOpen ?
-					<div className={styles.full}>
-						<div className={clsx(styles.grow, "card warning no-border", styles.note)}>
-							<div className={styles.row}>
-								<span className={styles.header}>Work In Progress</span>
-								<div className={styles.grow}></div>
-
-								<a href="#" onClick={(e) => {
-									setIsOpen(false);
-									e.preventDefault()
-								}}>
-									<Icon icon={Icons.close} height="1.5em" className="text-inverted"/>
-								</a>
-							</div>
-
-							<div>
-								This documentation is in beta. It's missing lots of content, search is
-								broken, and many links go nowhere.
-								These problems will be fixed before release, but there's plenty of work left!
-							</div>
-						</div>
-					</div> :
-					<></>
-			}
 
 			<Layout {...props} />
 		</>
